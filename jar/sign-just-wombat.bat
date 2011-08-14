c73d9fe3e01aea2aa27e190cb0ab75ffaf5267aa
johnpaul.verkamp <johnpaul.verkamp@07aa23ea-853d-4461-9545-5093946fcb7a>
2011-08-14 23:00:57 +0000

    - sign.bat now signs whatever bat drops on it rather than specific files - Main wombat jar has version in it's file name (to help with forced updating) - Options load correctly (why didn't they earlier? dunno)
    git-svn-id: https://wombat-ide.googlecode.com/svn/trunk@112 07aa23ea-853d-4461-9545-5093946fcb7a

--- jar/sign-just-wombat.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files\Java\jdk1.6.0_26\bin\jarsigner" -keystore .keystore -storepass password Wombat.jar myself
-pause
--- jar/sign.bat
+++ jar/sign.bat
@@ -1,4 +1,1 @@
-"C:\Program Files\Java\jdk1.6.0_26\bin\jarsigner" -keystore .keystore -storepass password Wombat.jar myself
-"C:\Program Files\Java\jdk1.6.0_26\bin\jarsigner" -keystore .keystore -storepass password lib/kawa-1.11.jar myself
-"C:\Program Files\Java\jdk1.6.0_26\bin\jarsigner" -keystore .keystore -storepass password lib/idw-gpl.jar myself
-pause
+"C:\Program Files\Java\jdk1.6.0_26\bin\jarsigner" -keystore .keystore -storepass password %1 myself
