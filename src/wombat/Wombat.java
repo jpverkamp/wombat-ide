1fac648894c5d9ca8e080dec71bfabe1d3d65ff2
johnpaul.verkamp <johnpaul.verkamp@07aa23ea-853d-4461-9545-5093946fcb7a>
2011-08-27 00:58:32 +0000

    - Didn't mean to commit the old ico file, just the png
    git-svn-id: https://wombat-ide.googlecode.com/svn/trunk@134 07aa23ea-853d-4461-9545-5093946fcb7a

--- src/wombat/Wombat.java
+++ src/wombat/Wombat.java
@@ -8,7 +8,7 @@ import javax.swing.*;
  * Main entry point of the program.
  */
 public class Wombat {
-	public static final int VERSION = 133;
+	public static final int VERSION = 134;
 	
     public static void main(String[] args) {
     	System.setSecurityManager(null);
