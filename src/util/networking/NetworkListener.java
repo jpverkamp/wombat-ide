8609a27b2da4c5900fc556df77f662200fd33374
JP Verkamp <verkampj@indiana.edu>
2011-10-05 21:04:40 -0400

    Removed networking from dev builds, it's in its own branch.

--- src/util/networking/NetworkListener.java
+++
@@ -1,9 +0,0 @@
-package util.networking;
-
-import java.net.Socket;
-
-public abstract class NetworkListener {
-	public abstract void onReceive(String content); 
-	
-	public abstract void onConnection(Socket client);
-}
