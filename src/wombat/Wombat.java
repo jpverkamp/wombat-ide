bc29bd3309807402161158a8ffc1bf51005159ca
JP Verkamp <verkampj@indiana.edu>
2011-10-13 22:05:41 -0400

    Adding modified main file as well.

--- src/wombat/Wombat.java
+++ src/wombat/Wombat.java
@@ -6,7 +6,7 @@ import gui.MainFrame;
  * Main entry point of the program.
  */
 public class Wombat {
-	public static final String VERSION = "{REVISION}";
+	public static final String VERSION = "{VERSION}";
 	
     public static void main(String[] argv) {
     	System.setSecurityManager(null);
