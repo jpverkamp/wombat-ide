2bf7e42139382877a5e514ceee23805880e39d42
JP Verkamp <verkampj@indiana.edu>
2011-10-18 14:35:07 -0400

    Fixed error where security manager was killing things.

--- src/wombat/Wombat.java
+++ src/wombat/Wombat.java
@@ -6,14 +6,15 @@ import gui.MainFrame;
  * Main entry point of the program.
  */
 public class Wombat {
-	public static final String VERSION = "{VERSION}";
+    public static final String VERSION = "{VERSION}";
 
-	public static void main(String[] argv) {
-		new Wombat();
-	}
+    public static void main(String[] argv) {
+	System.setSecurityManager(null);
+	new Wombat();
+    }
 
-	public Wombat() {
-		MainFrame main = new MainFrame();
-		main.setVisible(true);
-	}
+    public Wombat() {
+	MainFrame main = new MainFrame();
+	main.setVisible(true);
+    }
 }
