bc580c9b6c2556cb92671fc63fdb6b8a98e9af85
JP Verkamp <verkampj@indiana.edu>
2011-10-18 14:35:13 -0400

    Removed debug code.

--- src/util/KawaWrap.java
+++ src/util/KawaWrap.java
@@ -1,3 +1,4 @@
+
 package util;
 
 import java.util.HashMap;
@@ -113,7 +114,6 @@ public class KawaWrap {
 	 * @return The result.
 	 */
 	public String eval(String cmd) {
-		Throwable exc = null;
 		String err = null;
 		
 		try {
@@ -126,28 +126,21 @@ public class KawaWrap {
 				return formatObject(result);
 		
 		} catch (StackOverflowError ex) {
-			exc = ex;
 			err = "Possible infinite loop detected.";
 		
 		} catch (UnboundLocationException ex) {
-			exc = ex;
 			err = "Error: " + ex.getMessage().replace("location", "variable");
 		
 		} catch (WrongArguments ex) {
-			exc = ex;
 			err = "Error: " + ex.getMessage();
 		
 		} catch (IllegalArgumentException ex) {
-			exc = ex;
 			err = ex.getMessage();
 		
 		} catch (NamedException ex) {
-			exc = ex;
 			err = ex.toString();
 		
 		} catch (WrongType ex) {
-			exc = ex;
-			
 			if (("car".equals(ex.procname) || "cdr".equals(ex.procname)) && 
 					"()".equals(ex.argValue.toString()))
 				err = "Error in " + ex.procname + ": cannot take the " + ex.procname + " of an empty list.";
@@ -155,18 +148,13 @@ public class KawaWrap {
 				err = "Error: " + ex.toString();
 
 		} catch (RuntimeException ex) {
-			exc = ex;
 			err = "Error: " + ex.getMessage();
 		
 		} catch (Throwable ex) {
-			exc = ex;
 			ex.printStackTrace();
 			ErrorManager.logError("Unknown error handled (" + ex.getClass().getName() + "): " + ex.toString());
 			err = "Error: " + ex.getMessage();
 		}
-		
-		System.out.println("Caught:");
-		System.out.println(exc.getClass());
 		
 		err = err.replace(';', ',');
 		err = err.replace("<string>", "<repl>");
