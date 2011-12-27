ac59813b5cd38f114a6fa082334de51b7bfd8924
JP Verkamp <verkampj@indiana.edu>
2011-12-27 17:26:14 -0500

    Added display and write to base. This needs to be done better...

--- scheme/src/wombat/scheme/library/Base.java
+++ scheme/src/wombat/scheme/library/Base.java
@@ -30,5 +30,25 @@ public class Base {
 				return result;
 			}
 		});
+		
+		env.defineProcedure(new SchemeProcedure("display") {
+			private static final long serialVersionUID = -1287690738553847651L;
+
+			public SchemeObject<?> apply(SchemeObject<?>... args) { 
+				verifyExactArity(args.length, 1);
+				
+				return new SchemeString(args[0].display());
+			}
+		});
+		
+		env.defineProcedure(new SchemeProcedure("write") {
+			private static final long serialVersionUID = -75980137762828144L;
+
+			public SchemeObject<?> apply(SchemeObject<?>... args) { 
+				verifyExactArity(args.length, 1);
+				
+				return new SchemeString(args[0].write());
+			}
+		});
 	}
 }
