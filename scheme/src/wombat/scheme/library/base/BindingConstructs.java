dd9fc6cabfa50b988e4cdc4f38c1bb22a74ea084
JP Verkamp <verkampj@indiana.edu>
2012-01-06 22:31:29 -0500

    Started let.

--- scheme/src/wombat/scheme/library/base/BindingConstructs.java
+++ scheme/src/wombat/scheme/library/base/BindingConstructs.java
@@ -1,21 +1,15 @@
 package wombat.scheme.library.base;
 
-import java.util.Stack;
+import java.util.*;
 
-import wombat.scheme.Environment;
-import wombat.scheme.SExpression;
-import wombat.scheme.Tag;
-import wombat.scheme.errors.SchemeSyntaxError;
-import wombat.scheme.values.SchemeClosure;
-import wombat.scheme.values.SchemeMacro;
-import wombat.scheme.values.SchemeObject;
-import wombat.scheme.values.SchemeProcedure;
-import wombat.scheme.values.SchemeSymbol;
-import wombat.scheme.values.SchemeVoid;
+import wombat.scheme.*;
+import wombat.scheme.errors.*;
+import wombat.scheme.values.*;
 
 public class BindingConstructs {
 	@SuppressWarnings("serial")
 	public static void load(Environment env) {
+		// Set a variable. It must have been previously defined.
 		env.defineMacro(new SchemeMacro("set!") {
 			public void macroApply(
 					Stack<SExpression> sexps,
@@ -30,7 +24,7 @@ public class BindingConstructs {
 					verifyTypeOf(1, args[0], SchemeSymbol.class);
 				
 				// Push on a tag that will do that actual setting.
-				sexps.push(new SetBangTag((SchemeSymbol) args[0].getLiteral()));
+				sexps.push(new BindingTag((SchemeSymbol) args[0].getLiteral(), false));
 				envs.push(env);
 				
 				// Then push on the value to evaluate.
@@ -39,6 +33,7 @@ public class BindingConstructs {
 			}
 		});
 		
+		// Define a new variable in the current scope.
 		env.defineMacro(new SchemeMacro("define") {
 			public void macroApply(
 					Stack<SExpression> sexps,
@@ -54,7 +49,7 @@ public class BindingConstructs {
 					verifyTypeOf(1, args[0].getLiteral(), SchemeSymbol.class);
 					
 					// Push on a tag that will do that actual setting.
-					sexps.push(new DefineTag((SchemeSymbol) args[0].getLiteral()));
+					sexps.push(new BindingTag((SchemeSymbol) args[0].getLiteral(), true));
 					envs.push(env);
 					
 					// Then push on the value to evaluate.
@@ -138,6 +133,9 @@ public class BindingConstructs {
 			}
 		});
 		
+		// Two possibilities:
+		// - Simplest form of variable binding. Bind a series of variables that cannot refer to each other.
+		// - A named let which essentially binds a function to the let definition.
 		env.defineMacro(new SchemeMacro("let") {
 			public void macroApply(
 					final Stack<SExpression> sexps, 
@@ -147,6 +145,85 @@ public class BindingConstructs {
 					SExpression... args
 					) {
 
+				verifyMinimumArity(args.length, 2);
+				
+				SchemeSymbol name; // will be null if not a named let
+				List<SExpression> bindings;				
+				int firstBody;
+				
+				// Check if we are dealing with a normal let or a named let.
+				
+				// Named let
+				if (args[0].isLiteral()) {
+					verifyTypeOf(1, args[0].getLiteral(), SchemeSymbol.class);
+					name = (SchemeSymbol) args[0].getLiteral();
+					
+					if (!args[1].isList())
+						verifyTypeOf(2, args[1], SchemePair.class);
+					bindings = args[1].getList();
+					
+					firstBody = 2;
+				}
+				
+				// Regular let
+				else {
+					name = null;
+					
+					if (!args[0].isList())
+						verifyTypeOf(1, args[0], SchemePair.class);
+					bindings = args[0].getList();
+					
+					firstBody = 1;
+				}
+				
+				// Verify that all of the bindings are pairs of names and expressions and that there aren't any duplicates
+				for (int i = 0; i < bindings.size(); i++) {
+					if (bindings.get(i).isLiteral() || bindings.get(i).getList().size() != 2)
+						throw new SchemeSyntaxError(this, "Invalid binding at " + bindings.get(i).getLiteral().display() + ", expected variable name and expression.");
+					
+					if (bindings.get(i).getList().get(0).isLiteral())
+						verifyTypeOf(firstBody - 1, bindings.get(i).getList().get(0).getLiteral(), SchemeSymbol.class);
+					else
+						verifyTypeOf(firstBody - 1, bindings.get(i).getList().get(0), SchemeSymbol.class);
+					
+					if (((SchemeSymbol) bindings.get(i).getList().get(0).getLiteral()).isDot())
+						throw new SchemeSyntaxError(this, "A dot is not a valid name for a binding.");
+					
+					for (int j = 0; j < i; j++)
+						if (bindings.get(i).getList().get(0).getLiteral().equals(bindings.get(j).getList().get(0).getLiteral()))
+							throw new SchemeSyntaxError(this, "Duplicate identifier '" + bindings.get(i).getList().get(0).getLiteral().display() + "'");
+				}
+				
+				// Create the new environment that all of these bindings are going to get pushed into.
+				Environment newEnv = env.extend();
+				
+				// Push the bodies that are going to get evaluated in that new environment.
+				// Push them first so they get evaluated last.
+				// Each except for the last gets a value eater.
+				sexps.push(args[args.length - 1]);
+				envs.push(newEnv);
+				
+				for (int i = args.length - 2; i >= firstBody; i--) { 
+					sexps.push(new ValueEaterTag());
+					envs.push(newEnv);
+					
+					sexps.push(args[i]);
+					envs.push(newEnv);
+				}
+
+				// First a binder for each pair (to go after all the bindings are evaluated)
+				// Bindings are all in the new environment
+				for (SExpression bind : bindings) {
+					sexps.push(new BindingTag((SchemeSymbol) bind.getList().get(0).getLiteral(), true));
+					envs.push(newEnv);
+				}
+				
+				// Now evaluate all the bindings first
+				// Evaluations are all in the original environment
+				for (SExpression bind : bindings) {
+					sexps.push(bind.getList().get(1));
+					envs.push(env);
+				}
 			}
 		});
 		
@@ -212,15 +289,28 @@ public class BindingConstructs {
 	}
 }
 
-class SetBangTag extends Tag {
+/**
+ * Used to set or define variables.
+ */
+class BindingTag extends Tag {
 	private static final long serialVersionUID = -6219294329111768317L;
 	
 	SchemeSymbol Name;
+	boolean IsDefine;
 	
-	public SetBangTag(SchemeSymbol name) {
+	/**
+	 * Create a new set tag. 
+	 * @param name The name of the variable that will be set.
+	 * @param isDefine If the variable should be defined (it will be set if not).
+	 */
+	public BindingTag(SchemeSymbol name, boolean isDefine) {
 		Name = name;
+		IsDefine = isDefine;
 	}
 	
+	/**
+	 * Apply the tag.
+	 */
 	public void apply(
 			Stack<SExpression> sexps,
 			Stack<Environment> envs,
@@ -231,31 +321,22 @@ class SetBangTag extends Tag {
 		if (toSet instanceof SchemeProcedure)
 			((SchemeProcedure) toSet).setName(Name.getValue());
 		
-		env.set(Name, toSet);
+		if (IsDefine)
+			env.define(Name, toSet);
+		else
+			env.set(Name, toSet);
 		values.push(SchemeVoid.singleton());
 	}
 }
 
-class DefineTag extends Tag {
-	private static final long serialVersionUID = 2046247061891921114L;
-	
-	SchemeSymbol Name;
-	
-	public DefineTag(SchemeSymbol name) {
-		Name = name;
-	}
-	
-	public void apply(
-			Stack<SExpression> sexps,
-			Stack<Environment> envs,
-			Stack<SchemeObject<?>> values, 
-			Environment env) {
-		
-		SchemeObject<?> toSet = values.pop();
-		if (toSet instanceof SchemeProcedure)
-			((SchemeProcedure) toSet).setName(Name.getValue());
-		
-		env.define(Name, toSet);
-		values.push(SchemeVoid.singleton());
+/**
+ * Class to eat up (ignore) a single returned value.
+ */
+class ValueEaterTag extends Tag {
+	private static final long serialVersionUID = 8883620994897093516L;
+
+	@Override
+	public void apply(Stack<SExpression> sexps, Stack<Environment> envs, Stack<SchemeObject<?>> values, Environment env) {
+		values.pop();
 	}
 }
